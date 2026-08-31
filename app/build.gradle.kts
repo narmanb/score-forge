dd8dd33b87bbd1fdab7876672e9255c29d14be21
narmanb <43427228+narmanb@users.noreply.github.com>
2026-08-31 05:00:03 -0500

    Match FluidSynth shared C++ runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
         externalNativeBuild {
             cmake {
                 cppFlags += "-std=c++17"
+                arguments += "-DANDROID_STL=c++_shared"
             }
         }
     }
